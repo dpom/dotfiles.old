c24deb08bca4dc30f75a1ba6ea835515cd519bfe
Dan Pomohaci <dan.pomohaci@gmail.com>
2017-01-03 13:54:37 +0200

    adds lein-codeindex

--- .lein/profiles.small.clj
+++ .lein/profiles.small.clj
@@ -1,5 +1,6 @@
 {:user
  {:plugins [[lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
+            [lein-codeindex "0.1.0"]
             [lein-checkall "0.1.1" :exclusions [org.clojure/tools.namespace org.clojure/clojure]]
             [com.jakemccrary/lein-test-refresh "0.16.0"]]
                    :dependencies [[pjstadig/humane-test-output "0.8.0"]
