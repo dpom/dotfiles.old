f57f73888731d7939ed246e22f38c9b900d4c5a6
Dan Pomohaci <dan.pomohaci@gmail.com>
2017-07-27 10:33:49 +0300

    switch to cider

--- .lein/profiles.small.clj
+++ .lein/profiles.small.clj
@@ -1,8 +1,12 @@
-{:user
- {:plugins [[lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
-            [lein-codeindex "0.1.0"]
-            ;; [lein-checkall "0.1.1" :exclusions [org.clojure/tools.namespace org.clojure/clojure]]
-            ;; [venantius/ultra "0.5.1"]
-            ]
-  }}
- 
+{:user {:plugins [[lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
+                  [lein-codeindex "0.1.0"]
+                  ;; [lein-checkall "0.1.1" :exclusions [org.clojure/tools.namespace org.clojure/clojure]]
+                  ;; [venantius/ultra "0.5.1"]
+                  ]
+        }
+ :repl {:plugins [[cider/cider-nrepl "0.15.0"]
+                   [refactor-nrepl "2.3.1"]]
+         :dependencies [[alembic "0.3.2"]
+                        [org.clojure/tools.nrepl "0.2.13"]]
+         }
+ }
