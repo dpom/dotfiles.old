d8d69de06e0efab5c81753a31fb2789d15a54759
Dan Pomohaci <dan.pomohaci@gmail.com>
2017-09-12 06:22:38 +0300

    creates a reset snippet

--- .lein/profiles.small.clj
+++ .lein/profiles.small.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [
                   ;; [venantius/ultra "0.5.1"]
                   ]
+        :dependencies [[pjstadig/humane-test-output "0.8.2"]]
+        :injections [(require 'pjstadig.humane-test-output)]
         }
 }
