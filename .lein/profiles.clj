2c01e17a94fe67eeaf0e63d238a6028274e13ec8
Dan Pomohaci <dan.pomohaci@gmail.com>
2017-12-30 08:31:46 +0200

    clean lein config

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,8 @@
-/home/dan/pers/dotfiles/.lein/profiles.small.clj
+{:user {:plugins [
+                  ;; [venantius/ultra "0.5.1"]
+                  ]
+        :dependencies [[pjstadig/humane-test-output "0.8.2"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        }
+}
--- .lein/profiles.small.clj
+++
@@ -1,8 +0,0 @@
-{:user {:plugins [
-                  ;; [venantius/ultra "0.5.1"]
-                  ]
-        :dependencies [[pjstadig/humane-test-output "0.8.2"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]
-        }
-}
